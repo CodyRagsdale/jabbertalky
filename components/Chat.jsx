d9af34a1b09fa527100bd7a798ae827af1f66311
CodyRagsdale <119916333+CodyRagsdale@users.noreply.github.com>
2023-08-02 14:02:14 -0400

    Add giftedchat and update chat component

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,30 +1,70 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: `Hello ${name}!`,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: `Welcome to the chat, ${name}!`,
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: `${name}'s Chat` });
   }, []);
 
   return (
-    <View style={[styles.container, { backgroundColor: color }]}>
-      <Text style={styles.greeting}>Welcome to the chat, {name}!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={[styles.container, { backgroundColor: color }]}>
+        <GiftedChat
+          messages={messages}
+          onSend={(messages) => onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" && (
+          <KeyboardAvoidingView behavior="height" />
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  greeting: {
-    fontSize: 24,
-    textAlign: "center",
-    margin: 10,
   },
 });
 
